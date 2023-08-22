3856820cafe8a09172a310664b1e8aa02c3f96fb
ErickTH7 <erickthschool@gmail.com>
2023-08-22 11:50:58 -0500

    tarea1

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 - Nombre : Crlos Castañeda Ramirez
 - Correo : castr@uaz.edu.mx
 
+- p01-hola-mundo
+- p02-paga-trabajador
+- p03-venta-pizza
+
+para hacer programa es: dotnet new console -o "nombre"
---
+++ p02-paga-trabajador/Program.cs
@@ -0,0 +1,31 @@
+﻿// calcula la paga de un trabajador, considerando las horas extra
+
+string nombre = "";
+int horas = 0, hextra = 0;
+float paga = 0, pagaextra= 0, pagabruta = 0, paganeta = 0, impuesto = 0, tasa = 0;
+
+Console.Clear();
+Console.Write("nombre ? "); nombre = Console.ReadLine();
+Console.Write("Horas  ? "); horas = int.Parse(Console.ReadLine());
+Console.Write("Paga   ? "); paga = float.Parse(Console.ReadLine());
+
+//calculos
+tasa = 0.03f;
+
+if(horas > 40) {
+    hextra = horas - 40;
+    pagaextra = 2 * paga * hextra;
+    pagabruta = 40 * paga + pagaextra;
+}else
+    pagabruta = horas * paga;
+
+impuesto = pagabruta * tasa;
+paganeta = pagabruta - impuesto;
+
+//salida
+Console.WriteLine($"\nResumen de pagos del trabajador {nombre} \n");
+Console.WriteLine($"{horas} horas trabajadas, {hextra} extra, paga {paga} tasa de impuesto {tasa:p2}");
+Console.WriteLine($"paga bruta: {pagabruta,12:c2}");
+Console.WriteLine($"paga extra: {pagaextra,12:c2}");
+Console.WriteLine($"impuesto: {impuesto,12:c2}");
+Console.WriteLine($"paga neta: {paganeta,12:c2}");
